036016ea8c56e901c414c0384b6af0c91fcd072e
devaditya00 <adityamishra76731@gmail.com>
2026-04-05 01:29:50 +0530

    updated db.js for avoiding cold start

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
-import {ENV} from './env.js';
+import { ENV } from './env.js';
+
+let isConnected = false;
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(ENV.mongoUri);
-        console.log(`MongoDb connected: ${conn.connection.host}`);
-    } catch(err) {
+        if (isConnected) {
+            console.log("Using existing DB connection");
+            return;
+        }
+
+        const conn = await mongoose.connect(ENV.mongoUri, {
+            maxPoolSize: 10,
+            minPoolSize: 2,
+        });
+
+        isConnected = conn.connections[0].readyState === 1;
+
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (err) {
         console.error(`MongoDB connection failed: ${err.message}`);
         process.exit(1);
     }
 };
 
 export default connectDB;
-
